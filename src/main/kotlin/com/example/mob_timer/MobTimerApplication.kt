8f2edde20070ae3c7b2ed3e3101165eee87d5d94
0xmokurent <will1522sagacityfield@gmail.com>
2025-05-15 17:50:11 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/mob_timer/MobTimerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mob_timer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobTimerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MobTimerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/mob_timer/MobTimerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mob_timer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MobTimerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
